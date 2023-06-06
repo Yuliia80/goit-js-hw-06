319f5db8ba795930f2799bdfadd3e5c9701b8bb5
Yuliia80 <tretyak_1309@ukr.net>
2023-06-06 14:41:04 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,11 +1,10 @@
-const inputEl = document.querySelector('#name-input');
-const spanEl = document.querySelector('#name-output');
+const inputEl = document.querySelector("#name-input");
+const spanEl = document.querySelector("#name-output");
 
-inputEl.addEventListener('input', (event) => {
-  if (event.target.value === '') { 
-  spanEl.textContent = 'Anonymous'}
-  else {spanEl.textContent = event.target.value.trim()};
-  
+inputEl.addEventListener("input", (event) => {
+  if (event.target.value === "") {
+    spanEl.textContent = "Anonymous";
+  } else {
+    spanEl.textContent = event.target.value.trim();
+  }
 });
-
-
